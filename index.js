5f6177f864273586c316aad3d26a6c29679a0d29
Zein Hammad Marhabah <zeinmrhb@gmail.com>
2019-04-22 07:47:53 +0700

    change activity on todo-list and give line-through to one of them

--- index.js
+++ index.js
@@ -14,29 +14,48 @@ const newTodoList = () => {
 
   const span = document.createElement("span")
   const deleteButton = document.createTextNode("\u00D7")
-  span.className = "close"
+  span.className = "delete"
 
   span.appendChild(deleteButton)
   li.appendChild(span)
+
+  // for (let index = 0; index < close.length; index++) {
+  //   close[index].onclick = function () {
+  //     let parent = this.parentElement
+  //     parent.style.display = "none;"
+  //   }
+  // }
+
 }
+
+// const close = document.getElementsByClassName("delete")
+// for (let index = 0; index < close.length; index++) {
+//   close[index].onclick = function () {
+//     let parent = this.parentElement
+//     parent.style.display = "none;"
+//   }
+// }
+
+
+
 
 // Activate todo-list line-through style
 
 const list = document.querySelector('ul')
 list.addEventListener('click', function (event) {
   if (event.target.tagName === 'LI') {
-    event.target.classList.toggle('checked')
+    event.target.classList.toggle('done')
   }
 })
 
 // Give deleteButton to todo-list that already exist before
 
-let myTodolist = document.getElementsByTagName("LI");
+let myTodolist = document.getElementsByTagName("LI")
 let i;
 for (i = 0; i < myTodolist.length; i++) {
-  let span = document.createElement("span");
-  let deleteButton = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(deleteButton);
-  myTodolist[i].appendChild(span);
+  let span = document.createElement("span")
+  let deleteButton = document.createTextNode("\u00D7")
+  span.className = "delete"
+  span.appendChild(deleteButton)
+  myTodolist[i].appendChild(span)
 }
