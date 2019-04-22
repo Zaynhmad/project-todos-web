ba700247c3845453c6f4c4b0eb48a1685b1d56de
Zein Hammad Marhabah <zeinmrhb@gmail.com>
2019-04-22 11:49:27 +0700

    remove delete button

--- index.js
+++ index.js
@@ -5,39 +5,19 @@ const newTodoList = () => {
   const text = document.createTextNode(inputValue)
   const li = document.createElement("li")
   li.appendChild(text)
-  if (inputValue === '' || inputValue === ` `) {
+  if (inputValue === "" || inputValue === ` `) {
     alert("Write something to do!")
   } else {
-    document.getElementById("todo-list").appendChild(li);
+    document.getElementById("todo-list").appendChild(li)
   }
   document.getElementById("input").value = ""
-
-  const span = document.createElement("span")
-  const deleteButton = document.createTextNode("\u00D7")
-  span.className = "delete"
-
-  span.appendChild(deleteButton)
-  li.appendChild(span)
-
 }
 
 // Activate todo-list line-through style
 
-const list = document.querySelector('ul')
-list.addEventListener('click', function (event) {
-  if (event.target.tagName === 'LI') {
-    event.target.classList.toggle('done')
+const list = document.querySelector("ul")
+list.addEventListener("click", function (event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("done")
   }
 })
-
-// Give deleteButton to todo-list that already exist before
-
-let myTodolist = document.getElementsByTagName("LI")
-let i;
-for (i = 0; i < myTodolist.length; i++) {
-  let span = document.createElement("span")
-  let deleteButton = document.createTextNode("\u00D7")
-  span.className = "delete"
-  span.appendChild(deleteButton)
-  myTodolist[i].appendChild(span)
-}
